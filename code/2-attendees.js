a9d91d64039580fa78076fcaeda5ed16aeaf806c
Varuni <nadikuva57@craighead.school.nz>
2023-07-10 13:53:39 +1200

    commit

--- code/2-attendees.js
+++ code/2-attendees.js
@@ -8,9 +8,8 @@ function attendees() {
   let attendeeList = ''
   
   while (count < numOfAttendees) {
-    let attendee = prompt()
-    attendeeList = attendeeList + attendee
-    attendeeList = attendeeList + ', '
+    let attendee = prompt("Enter a name:")
+    attendeeList = attendeeList + ', ' + attendee
     count = count + 1
   }
   
@@ -18,6 +17,14 @@ function attendees() {
 }
 
 function make2() {
-  // copy and paste your code here when you get to the Make step
+  let count = 0
+  let numOfNumbers = prompt("How many numbers do you want to add?")
   
+  while (count < numOfNumbers) {
+    let number = prompt("Enter a number:")
+    count = count + 1
+    number = number + number
+  }
+
+  alert(number) 
 }
